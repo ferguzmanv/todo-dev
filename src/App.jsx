9139211f617317687429ccf7613dca1acd441be1
Fernanda Guzman <fda.guzmann@gmail.com>
2023-05-27 02:17:35 -0400

    imagen fondo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ const App = () => {
  
   return (
 
-
-  <div className="bg-[url('./assets/images/bg-mobile-light.jpg')">
+    // bg-[url('assets/images/bg-mobile-light.jpg')
+  <div className= "bg-[url('assets/images/bg-mobile-light.jpg')] bg-cover bg-center bg-no-repeat">
     <h1 className="uppercase">HOLI todo's </h1>
     <button>luna</button>
   </div>
