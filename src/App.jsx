2ca72a6ba085210275651f2bfff0a25044968bc7
Fernanda Guzman <fda.guzmann@gmail.com>
2023-05-27 01:40:39 -0400

    imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 const App = () => {
-  return <h1 className="text-center">HOLI</h1>
 
+ 
+  return (
+
+
+  <div className="bg-[url('./assets/images/bg-mobile-light.jpg')">
+    <h1 className="uppercase">HOLI todo's </h1>
+    <button>luna</button>
+  </div>
+  );
 };
 
 export default App;
