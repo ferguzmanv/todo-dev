885b6907f3fbb0b6c89c9d2119ef1fecd6b3942a
Fernanda Guzman <fda.guzmann@gmail.com>
2023-05-28 02:05:15 -0400

    lista todo en container

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,62 @@
+import React from 'react';
+import xIcon from "./components/xIcon";
+
 const App = () => {
-
-
   return (
 
     <div className="bg-[url('assets/images/bg-mobile-light.jpg')] 
-    bg-contain bg-center bg-no-repeat">
+    bg-contain bg-center bg-no-repeat bg-slate-200 ">
       <header className="contain mx-auto px-4">
-        
+
         <div className="flex justify-between">
-          <h1 className="uppercase text-white text-3xl font-bold">To do's </h1>
+          <h1 className="uppercase text-white text-3xl font-bold tracking-[0.1em] m-6" >To do's </h1>
           <button>luna</button>
-          </div>
+        </div>
 
 
-        <form className="overflow-hidde rounded-md bg-white py-4 gap-4">
-          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
-          <input type="text" placeholder="Create a new todo" />
+        <form className="flex items-center overflow-hidde rounded-md bg-white py-2 m- gap-4">
+          <span className="inline-block h-5 w-5 rounded-full border-2 mx-3 "></span>
+          <input
+            type="text"
+            placeholder="Create a new todo"
+            className="w-full border-none text-gray-400 outline-none" />
         </form>
       </header>
-      
-      <main className="container mx-auto px-4">
-        <article>
-          <button>circle</button>
-          <p>Estudiar AWS</p>
-          <button>EXISDE</button>
-        </article>
-        <article>
-          <button>circle</button>
-          <p>Estudiar 2</p>
-          <button>EXISDE</button>
-        </article>
-        <article>
-          <button>circle</button>
-          <p>Estudiar 3</p>
-          <button>EXISDE</button>
-        </article>
-    <section>
-      <span>3 items left</span>
-      <button>Clear completed</button>
-    </section>
 
-    </main>
-    <section className="container mx-auto px-4">
-      <button>all</button>
-      <button>active</button>
-      <button>completed</button>
-    </section>
+      <main className="container mx-auto mt-10 px-4">
+        <div className="rounded-md bg-white px-4">
+          <article>
+            <button className="inline-block h-5 w-5 rounded-full border-2 mx-3"></button>
+            <p className="text-gray-500">Estudiar AWS</p>
+            <button>
+            <xIcon />
+            </button>
 
-  </div>
+          </article>
+          <article>
+            <button>circle</button>
+            <p>Estudiar 2</p>
+            <button>EXISDE</button>
+          </article>
+          <article>
+            <button>circle</button>
+            <p>Estudiar 3</p>
+            <button>EXISDE</button>
+          </article>
+          <section>
+            <span>3 items left</span>
+            <button>Clear completed</button>
+          </section>
+        </div>
+      </main>
+
+      <section className="container mx-auto px-4">
+        <button>all</button>
+        <button>active</button>
+        <button>completed</button>
+      </section>
+
+    </div>
   );
 };
 
